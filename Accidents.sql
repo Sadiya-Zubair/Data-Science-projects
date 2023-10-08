22690a2b134ee49058381e70b85b1bf6a52c0c58
Sadiya Zubair <36756199+Sadiya-Zubair@users.noreply.github.com>
2023-10-09 04:38:22 +0530

    Update Accidents.sql

--- Accidents.sql
+++ Accidents.sql
@@ -15,31 +15,6 @@ SELECT
 FROM "Accidents".accidents
 WHERE "Number" != 0 AND "Death rate per 100 000 population" != 0;
 
-
---Top 10 countries with Maximum death rate over 100,000 deaths
-with a as (SELECT
-    "Country Name",
-    "Age Group",
-    "Sex",	   
-    "Number",
-    "Year",
-    "Death rate per 100 000 population" as "death rate", 
-    CASE
-        WHEN "Death rate per 100 000 population" = 0 THEN 0  
-        ELSE ROUND(("Number" / "Death rate per 100 000 population") * 100000)
-    END AS "Total population"
-FROM "Accidents".accidents
-WHERE "Number" != 0 AND "Death rate per 100 000 population" != 0),
-b as (select "Country Name",(sum("Number") / sum("Total population") * 100000) as "Death Rate" from a
-GROUP BY "Country Name")
-SELECT
-    "Country Name",
-    Max("Death Rate") AS "Max Death Rate"
-FROM b
-GROUP BY "Country Name"
-order by "Max Death Rate" Desc
-limit 10;
-
 --Top 10 countries with Maximum death rate over 100,000 deaths each year
 with a as (SELECT
     "Country Name",
